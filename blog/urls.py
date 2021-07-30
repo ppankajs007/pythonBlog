4f7e4368fb4e4aa9ee69100f7785c7d426bbd2a0
ppankajs007 <ppankajs007@gmail.com>
2021-07-30 21:18:27 +0530

    Add Login and profile layout, profile update pending

--- blog/urls.py
+++ blog/urls.py
@@ -20,5 +20,8 @@ from blog import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('login',views.login,name='login'),
-    path('register',views.register,name='register')
+    path('register',views.register,name='register'),
+    path('profile', views.profile, name='profile'),
+    path('logout', views.logout,name='logout')
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate,login as userLogin,logout as userLogout
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
 
 def login(request):
+    if request.method == 'POST':
+        if request.POST.get('username') == '' or request.POST.get('password') == '':
+            messages.warning(request,'Required all fields *')
+            return redirect('login')
+        else:
+            user = authenticate(request,username=request.POST.get('username'),password=request.POST.get('password'))
+            if user is not None:
+                userLogin(request,user)
+                return redirect('profile')
+            else:
+                messages.warning(request,'Username and password is wrong')
+                return redirect('register')
+    else:
         return render(request,'login.html')
+
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request,'profile.html')
+    else:
+        return redirect('login')
+
+
+def logout(request):
+    userLogout(request)
+    return redirect('/')
+
 
 
 def register(request):
@@ -19,20 +46,26 @@ def register(request):
             email = request.POST.get('email')
             password = request.POST.get('password')
             confirm_password = request.POST.get('confirm_password')
-            if User.objects.filter(email=email).exists():
-                error = 1
-                messages.warning(request,'Email is already exist')
-            if User.objects.filter(username=username).exists():
-                error = 1
-                messages.warning(request, 'Username is already exist')
-            if password != confirm_password:
-                error = 1
-                messages.warning(request, 'Password dont match')
-            if error == 1:
+            if firstname == '' or lastname == '' or username == '' or email == '' or password == '':
+                messages.warning(request, 'Required all fields *')
                 return redirect('register')
             else:
-                user = User(first_name=firstname,last_name=lastname,username=username,email=email,password=password)
-                user.save()
-                return redirect('index')
+                if User.objects.filter(email=email).exists():
+                    error = 1
+                    messages.warning(request,'Email is already exist')
+                if User.objects.filter(username=username).exists():
+                    error = 1
+                    messages.warning(request, 'Username is already exist')
+                if password != confirm_password:
+                    error = 1
+                    messages.warning(request, 'Password dont match')
+                if error:
+                    return redirect('register')
+                else:
+                    user = User(first_name=firstname,last_name=lastname,username=username,email=email,password=make_password(password))
+                    user.save()
+                    userDetails = authenticate(request, username=username,password=password)
+                    userLogin(request,userDetails)
+                    return redirect('profile')
         else:
             return render(request, 'register.html')
